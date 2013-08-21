c965b64d3ae66934acece9e2990635cf83f08939
Gerard Hedger <gerard.hedger@gmail.com>
2013-08-21 21:48:45 +1000

    Inital 	new file:   Zipper.sln 	new file:   Zipper.suo 	new file:   Zipper/Program.cs 	new file:   Zipper/Properties/AssemblyInfo.cs 	new file:   Zipper/Zipper.csproj 	new file:   Zipper/bin/Debug/C1.C1Zip.4.dll 	new file:   Zipper/bin/Debug/C1.C1Zip.4.xml 	new file:   Zipper/bin/Debug/Zipper.exe 	new file:   Zipper/bin/Debug/Zipper.pdb 	new file:   Zipper/bin/Debug/Zipper.vshost.exe 	new file:   Zipper/bin/Debug/Zipper.vshost.exe.manifest 	new file:   Zipper/bin/Debug/Zipper.zzz 	new file:   zipper/obj/x86/Debug/esignTimeResolveAssemblyReferencesInput.cache 	new file:   Zipper/obj/x86/Debug/Zipper.csproj.FileListAbsolute.txt 	new file:   Zipper/obj/x86/Debug/Zipper.exe 	new file:   Zipper/obj/x86/Debug/Zipper.pdb

---
+++ Zipper/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using C1.C1Zip;
+
+namespace Zipper
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            String DirectoryToRead = "";
+            String DirectoryToSave = "";
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: Zipper \\SourceFolder \\DestinationFile");
+                return;
+            }
+            if (args.Length > 0)
+            {
+                Console.WriteLine(args[0]);
+                DirectoryToRead = args[0];
+
+            }
+            if (args.Length > 1)
+            {
+                DirectoryToSave = args[1] + DateTime.Now.ToString("yyyyMMddHHmm") + ".zip";
+                Console.WriteLine(args[1] + DateTime.Now.ToString("yyyyMMddHHmm") + ".zip");
+            }
+
+            Console.WriteLine(DirectoryToSave);
+
+
+            Int32 Depth = 0;
+            C1ZipFile zf = new C1ZipFile(DirectoryToSave, true);
+            FolderLoop(DirectoryToRead, Depth, zf);
+            zf.Close();
+            //Console.WriteLine("Press any key ...");
+            //Console.ReadKey();
+
+        }
+
+        static void FolderLoop(String DirectoryToRead, Int32 Depth, C1ZipFile zf)
+        {
+            DirectoryInfo di = new DirectoryInfo(DirectoryToRead);
+            ++Depth;
+
+            foreach (DirectoryInfo diSub in di.GetDirectories())
+            {
+                Console.WriteLine(DirectoryToRead);
+                DirectoryToRead = diSub.FullName;
+                FolderLoop(DirectoryToRead, Depth, zf);
+            }
+            foreach (FileInfo fi in di.GetFiles())
+            {
+                Console.WriteLine(fi.FullName);
+                zf.Entries.Add(fi.FullName, Depth);
+            }
+        }
+
+
+
+    }
+}
